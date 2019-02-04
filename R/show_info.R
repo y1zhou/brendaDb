5aa371b639f941d8f48d2b8f7d1fccfbf50cbee1
y1zhou <zhou.zy.yi@gmail.com>
2019-02-04 18:05:31 -0500

    feat(describe-txt): Show information about the fields.

---
+++ R/show_info.R
@@ -0,0 +1,19 @@
+#' @title Show all unique BRENDA fields and their corresponding acronyms.
+#'
+#' @param df A data.frame with columns "field" and "description"
+#'
+#' @return A data.frame with columns "field" and "acronym".
+#'
+#' @export
+show.fields <- function(df) {
+  if (missing(df)) {
+    stop("Missing parameter df. If you want all the fields, please use data(acronyms).")
+  }
+  else {
+    df_field <- unique(df$field)
+    acronyms <- df[!duplicated(df$field), ]
+    acronyms$acronym <- sub("^([A-Z]+)\t.*$", "\\1", acronyms$description)
+    acronyms <- acronyms[ ,c("field", "acronym")]
+  }
+  return(acronyms)
+}
