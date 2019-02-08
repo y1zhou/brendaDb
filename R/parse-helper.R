4dc117321068063cb832b0075ce98cb5edfb1a13
y1zhou <zhou.zy.yi@gmail.com>
2019-02-08 18:05:07 -0500

    feat(parser): Function for parsing the field PROTEIN.
    Break line into a named list of lists.

---
+++ R/parse-helper.R
@@ -0,0 +1,47 @@
+#' @title Parse protein information strings or reference strings.
+#'
+#' @description Given a string like "#1,45,72#", parse into a character vector
+#' of c("1", "45", "72").
+#'
+#' @param ref A string in the format of "#1#" or "#1,2,3#" or "<1,3>".
+#' @param type Either "protein" or "reference".
+#'
+#' @return A string, or a vector of strings of protein numbers.
+#'
+#' @examples
+#' parse.protein.num("#1,2,3#")
+#' # [1] "1,2,3"
+#' parse.protein.num("<123>")
+#' # [1] "123"
+#'
+#'@importFrom stringr str_glue
+ParseProteinNum <- function(x, type) {
+  if (missing(type)) {
+    stop("Missing parameter: type.")
+  }
+  if (type == "protein") {
+    delim <- "#"
+    if (!(grepl("^#(\\d+,)*\\d+#$", x))) {
+      stop(str_glue('"{x}" is not a valid protein string. ',
+                    "Please check brendaDb:::ParseProteinNum"))
+    }
+  } else if(type == "reference") {
+    delim <- "<|>"
+    if (!(grepl("^<(\\d+,)*\\d+>$", x))) {
+      stop(str_glue('"{x}" is not a valid reference string. ',
+                    "Please check brendaDb:::ParseProteinNum"))
+    }
+  } else {
+    stop(str_glue('Unknown value for parameter type: "{type}".'))
+  }
+
+  # Sanity check finished, now parse the string
+  x <- gsub(delim, "", x)
+  if (grepl("^\\d+$", x)) {
+    return(x)
+  } else {
+    return(strsplit(x, ",")[[1]])
+  }
+}
+
+
