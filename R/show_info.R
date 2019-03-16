dafcf259b3056c7c26139f13ee19323ad733925c
y1zhou <zhou.zy.yi@gmail.com>
2019-03-16 18:03:59 -0400

    perf(show-info): Use the tidyverse suite for displaying acronym information.

--- R/show_info.R
+++ R/show_info.R
@@ -4,15 +4,19 @@
 #'
 #' @return A data.frame with columns "field" and "acronym".
 #'
+#' @importFrom dplyr distinct mutate select
+#' @importFrom stringr str_extract
+#' @importFrom tibble as_tibble
 #' @export
 ShowFields <- function(df) {
   if (missing(df)) {
     stop("Missing parameter df. If you want all the fields, please use data(acronyms).")
   } else {
-    df_field <- unique(df$field)
-    acronyms <- df[!duplicated(df$field), ]
-    acronyms$acronym <- sub("^([A-Z05]+)\t.*$", "\\1", acronyms$description)
-    acronyms <- acronyms[ ,c("field", "acronym")]
+    acronyms <- df %>%
+      distinct(field, .keep_all = T) %>%
+      mutate(acronym = str_extract(description, "^[A-Z05]+")) %>%
+      select(field, acronym) %>%
+      as_tibble()
   }
   return(acronyms)
 }
