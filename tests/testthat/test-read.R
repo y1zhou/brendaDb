18ff97c536d1f7f27081bac06c5e12814816ae8f
y1zhou <zhou.zy.yi@gmail.com>
2019-04-09 14:37:32 -0400

    feat(read-txt): Clean up the EC numbers in the read-in text file.
    In the ID column, there are entries with commentaries about the status of the EC number (none,
    transferred, deleted, etc.). The function `CleanECNumber` extracts these entries and move the
    commentaries to the description column, giving the field column a value of "TRANSFERRED_DELETED".

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -3,13 +3,13 @@ context("Read BRENDA txt")
 test_that("Read BRENDA txt file correctly into matrix ", {
   brenda_txt <- system.file("extdata", "brenda_download_test.txt",
                             package = "brendaDb")
-  df <- ReadBrenda(brenda_txt)
+  df <- ReadBrenda(brenda_txt, clean = T)
   expect_error(ReadBrenda("fakefile.txt"))
   expect_is(df, "tbl_df")
   expect_equal(colnames(df), c("ID", "field", "description"))
 
   # First column should be EC numbers
-  expect_equal(unique(df$ID), "1.1.1.1")
+  expect_match(unique(df$ID), "^(\\d+\\.){3}\\d+$")
   # Second column should be field full names
   expect_match(unique(df$field), "^[A-Z_05]+$")
 })
