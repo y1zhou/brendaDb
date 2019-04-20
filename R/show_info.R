5d930c7843f73857ef1dfe9ec2b845bf78210318
y1zhou <zhou.zy.yi@gmail.com>
2019-04-19 22:58:12 -0400

    fix(show-info): Change to a lighter gray color.
    fix #8

--- R/show_info.R
+++ R/show_info.R
@@ -29,14 +29,19 @@ ShowFields <- function(df) {
 #' @param x A brenda.entries list returned by [QueryBrenda()].
 #'
 #' @import stringr
+#' @importFrom purrr map
 #' @export
-summary.brenda.entries <- function(x) {
+summary.brenda.entries <- function(x, verbose = F) {
   print(str_glue(
-    "A list of {length(x)} brenda.entry objects with ",
-    "{length(x[is.brenda.deprecated.entry(x)])} transferred or deleted:\n",
-    paste(names(x)[is.brenda.deprecated.entry(x)], collapse = ", "),
-    "\nand {length(x[!is.brenda.deprecated.entry(x)])} regular brenda.entry objects:\n",
-    paste(names(x[!is.brenda.deprecated.entry((x))]), collapse = ", ")))
+    "A list of {length(x)} brenda.entry object(s) with:\n",
+    "- {length(x[!is.brenda.deprecated.entry(x)])} regular brenda.entry object(s)\n  ",
+    paste(names(x)[!is.brenda.deprecated.entry(x)], collapse = ", "),
+    "\n- {length(x[is.brenda.deprecated.entry(x)])} transferred or deleted object(s)\n  ",
+    paste(names(x[is.brenda.deprecated.entry((x))]), collapse = ", ")))
+
+  if (verbose) {
+    invisible(map(x, summary))
+  }
 }
 
 
@@ -45,7 +50,6 @@ summary.brenda.entries <- function(x) {
 #' @param x A brenda.entry object (elements in the list returned by [QueryBrenda()]).
 #'
 #' @import stringr
-#' @importFrom crayon silver red
 #' @importFrom tibble is_tibble
 #' @importFrom purrr pmap
 #' @export
@@ -54,17 +58,18 @@ summary.brenda.entry <- function(x) {
     print(str_glue(
       "Entry {x$nomenclature$ec}\n",
       "├── nomenclature\n",
-      "|    └── ec\n",
-      "└── msg: {crayon::silver(x$msg)}"
+      "|    └── ec: {make_style(rgb(0.58, 0.58, 0.58))(x$nomenclature$ec)}\n",
+      "└── msg: {make_style(rgb(0.58, 0.58, 0.58))(x$msg)}"
     ))
   } else {
     cat("Entry", x$nomenclature$ec)
     invisible(pmap(list(x = x, i = names(x), tail.i = tail(names(x), 1)),
          function(x, i, tail.i) PrettyPrintBrendaEntry(x, i, tail.i, 0)))
+    cat("\n")
   }
 }
 
-
+#' @importFrom crayon make_style red
 PrettyPrintBrendaEntry <- function(x, index, tail.idx, depth) {
   if(index == tail.idx) {
     cat("\n", rep("|    ", depth), "└── ", index, sep = "")
@@ -76,10 +81,11 @@ PrettyPrintBrendaEntry <- function(x, index, tail.idx, depth) {
     pmap(list(x = x, i = names(x), tail.i = tail(names(x), 1)),
          function(x, i, tail.i) PrettyPrintBrendaEntry(x, i, tail.i, depth+1))
   } else if(is_tibble(x)) {
-    cat(":", crayon::silver("A tibble with", nrow(x), "rows"))
+    cat(":", make_style(rgb(0.58, 0.58, 0.58))("A tibble with", nrow(x), "rows"))
   } else if (is.na(x)) {
     cat(":", crayon::red("NA"))
   } else {
-    cat(":", crayon::silver(x))
+    cat(":", make_style(rgb(0.58, 0.58, 0.58))(x))
   }
 }
+
