b73ecba4255642c07b148ca8667da9f8d7a683dd
y1zhou <zhou.zy.yi@gmail.com>
2019-06-06 01:24:17 -0400

    test(show-info): Test cases for brenda.entry print functions.

--- tests/testthat/test-show_info.R
+++ tests/testthat/test-show_info.R
@@ -1,12 +1,29 @@
 context("Show information about BRENDA")
 
-test_that("Show BRENDA fields and acronyms ", {
+brenda_txt <- system.file("extdata", "brenda_download_test.txt",
+                          package = "brendaDb")
+df <- ReadBrenda(brenda_txt)
+
+test_that("Show BRENDA fields and acronyms", {
   expect_error(ShowFields())
-  brenda_txt <- system.file("extdata", "brenda_download_test.txt",
-                            package = "brendaDb")
-  df <- ReadBrenda(brenda_txt)
   df <- ShowFields(df)
 
   expect_match(unique(df$field), "^[A-Z_05]+$")
   expect_match(unique(df$acronym), "^[A-Z05]+$")
+})
+
+test_that("Printing brenda.query objects", {
+  x <- QueryBrenda(df, EC = c("1.1.1.1", "6.3.5.8"),
+                   n.core = 1, organisms = "Homo sapiens")
+  expect_output(print(x), regexp = "list of 2.+1 regular.+1 transferred")
+  expect_output(print(x, verbose = T),
+                regexp = "list of 2.+1 regular.+1 transferred.+\\|")
+
+  # There should be NA and "0 rows" in the output; both are colored red
+  x$`1.1.1.1`$nomenclature$reaction <- NA
+  expect_output(print(x$`1.1.1.1`),
+                regexp = ".+\\b(NA).+0 rows")
+
+  # Deprecated entries have different outputs
+  expect_output(print(x$`6.3.5.8`), regexp = "^Entry.+msg: transferred")
 })
