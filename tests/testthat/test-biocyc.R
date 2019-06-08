3531963080168bb77b66d26c2b123e3aa566c495
y1zhou <zhou.zy.yi@gmail.com>
2019-06-08 00:33:19 -0400

    test(biocyc): Test cases for querying BioCyc pathways.

---
+++ tests/testthat/test-biocyc.R
@@ -0,0 +1,25 @@
+context("Retrieve information from BioCyc")
+
+test_that("Get enzymes in BioCyc pathway ", {
+  expect_error(BiocycPathwayEnzymes())
+  expect_error(BiocycPathwayEnzymes(org.id = "HUMAN", pathway = NA))
+  expect_message(BiocycPathwayEnzymes(org.id = "HUMAN", pathway = "PWY66666"))
+  expect_message(
+    {x <- BiocycPathwayEnzymes(org.id = "HUMAN", pathway = "PWY66-400")},
+    regexp = "Found 10.+HUMAN.+PWY66-400"
+  )
+  expect_equal(dim(x), c(11, 2))
+  expect_match(x$EC, "^(\\d+\\.){3}\\d+$")
+})
+
+test_that("Get genes in BioCyc pathway ", {
+  expect_error(BiocycPathwayGenes())
+  expect_error(BiocycPathwayGenes(org.id = "HUMAN", pathway = NA))
+  expect_message(BiocycPathwayGenes(org.id = "HUMAN", pathway = "PWY66666"))
+  expect_message(
+    {x <- BiocycPathwayGenes(org.id = "HUMAN", pathway = "PWY66-400")},
+    regexp = "Found 25.+HUMAN.+PWY66-400"
+  )
+  expect_equal(dim(x), c(25, 4))
+  expect_match(x$Ensembl, "^ENSG\\d{11}")
+})
